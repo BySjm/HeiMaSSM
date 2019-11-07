7e91dba0dd3ad8902b7c268f10ee45de2932bc43
sanjiaomaojl <1026845550@qq.com>
2019-11-07 23:01:18 +0800

    practice

--- Day02SpringAnnoCRUDPRO/src/main/java/cn/bysjm/config/SpringConfigPractice.java
+++ Day02SpringAnnoCRUDPRO/src/main/java/cn/bysjm/config/SpringConfigPractice.java
@@ -1,5 +1,15 @@
 package cn.bysjm.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
 
+@Configuration
+@ComponentScan("cn.bysjm")
+@PropertySource("classpath:jdbc.properties")
 public class SpringConfigPractice {
+
+//    @Bean
+//    @Autowired
+//    @Import({DataSourceConfig.class})
+
 }
